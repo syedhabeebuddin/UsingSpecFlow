84dfb3a82127adba4fbf6d03ec7b240884f7eefc
syedhabeebuddin <syed@adtechcorp.in>
2019-08-19 21:53:44 +0530

    Add project files.

---
+++ Foo.Spec/Feature1Steps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Foo.Spec
+{
+    [Binding]
+    public class Feature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Add("kk", p0);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
